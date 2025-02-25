d1fbce5360e4e1a726370a6cb61e27d18cf46a5c
sasha-melok <nan11556600@gmail.com>
2025-02-25 03:54:40 +0000

    goodbye view

--- note-app/app/Http/Controllers/WelcomeController.php
+++ note-app/app/Http/Controllers/WelcomeController.php
@@ -11,4 +11,9 @@ class WelcomeController extends Controller
     {
         return view('welcome');
     }
+
+    public function goodbye()
+    {
+        return view('goodbye');
+    }
 }
--- note-app/routes/web.php
+++ note-app/routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 
 Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
+Route::get('/goodbye', [WelcomeController::class, 'goodbye'])->name('goodbye');
