acb4c74035d456a4127ae98bba6f9690ddc64877
sasha-melok <nan11556600@gmail.com>
2025-02-25 05:27:30 +0000

    note+todo created

--- note-app/database/seeders/DatabaseSeeder.php
+++ note-app/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Note;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +15,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        //User::factory(10)->create();
 
         User::factory()->create([
+            // Пока что хардкодим айдишку
+            'id' => 1,
             'name' => 'Test User',
             'email' => 'test@example.com',
+            // и хардкодим пароль
+            'password' => bcrypt('pass123.')
         ]);
+
+        // Создаем 100 заметок для теста
+        Note::factory(100)->create();
     }
 }
