72e389e81bf0f1420c9952d73ce811ec3dddc212
sasha-melok <nan11556600@gmail.com>
2025-03-15 11:46:18 +0000

    working but only with crutches

--- note-app/routes/web.php
+++ note-app/routes/web.php
@@ -11,17 +11,17 @@ Route::get('/goodbye', [WelcomeController::class, 'goodbye'])->name('goodbye');
 // note
 Route::resource('note', NoteController::class);
 // to do
-// Route::resource('todo', TodoController::class);
-
-Route::get('/todo/{id}', [TodoController::class, 'show'])->name('todo.show');
-Route::get('todo/{id}/edit', [TodoController::class, 'edit'])->name('todo.edit');
-Route::resource('todo', TodoController::class);
+// Route::resources(['todos' => TodoController::class, 'notes' => NoteController::class]);
 
 
 //Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
 //Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
 //Route::post('/todo', [TodoController::class, 'store'])->name('todo.store');
-//Route::get('/todo/{id}', [TodoController::class, 'show'])->name('todo.show');
-//Route::get('/todo/{id}/edit', [TodoController::class, 'edit'])->name('todo.edit');
-//Route::put('/todo/{id}', [TodoController::class, 'update'])->name('todo.uptododate');
-//Route::delete('/todo/{id}', [TodoController::class, 'destroy'])->name('todo.destroy');
+
+Route::get('/todo/{id}', [TodoController::class, 'show'])->name('todo.show');
+Route::get('/todo/{id}/edit', [TodoController::class, 'edit'])->name('todo.edit');
+Route::put('/todo/{id}', [TodoController::class, 'update'])->name('todo.uptododate');
+Route::delete('/todo/{id}', [TodoController::class, 'destroy'])->name('todo.destroy');
+
+
+Route::resource('todo', TodoController::class);
